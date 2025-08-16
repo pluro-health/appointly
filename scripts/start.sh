6576ad727eeb8fd3827966e589152f476e9ca931
Tejas Karelia <tejas.karelia17@gmail.com>
2025-08-16 10:36:59 -0700

    Cleaup Docker

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -x
 
+# Replace the statically built BUILT_NEXT_PUBLIC_WEBAPP_URL with run-time NEXT_PUBLIC_WEBAPP_URL
+# scripts/replace-placeholder.sh "$BUILT_NEXT_PUBLIC_WEBAPP_URL" "$NEXT_PUBLIC_WEBAPP_URL"
+
 # Optional init script (creates DB if missing, seeds, etc.)
 if [ -f "/appointly/init-db.sh" ]; then
   echo "🗄️  Running database initialization..."
